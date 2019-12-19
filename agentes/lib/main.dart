c89cacb1a4be8ca8c6767a6de0efa0547a9a5d0d
cristian alvarado esperano <Cris_ales@LIVE.COM.MX>
2019-12-19 14:04:35 -0700

    Se quitaron errores de librerias deprecated

--- agentes/lib/main.dart
+++ agentes/lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:agentes/src/app/app.dart';
-import 'package:agentes/src/preferencias_usuario/preferencias_usuario.dart';
 
 void main() async{
-    
-  final prefs = new PreferenciasUsuario();
-  await prefs.initPrefs();
   runApp(MyApp());
 } 
  
--- agentes/lib/src/blocs/provider.dart
+++ agentes/lib/src/blocs/provider.dart
@@ -3,37 +3,34 @@ import 'login_bloc.dart';
 
 import 'package:agentes/src/blocs/pedidos_bloc.dart';
 
-
-
 class Provider extends InheritedWidget {
-
   final _loginBloc = new LoginBloc();
   final _pedidosBloc = new PedidosBloc();
 
   static Provider _instancia;
 
   factory Provider({Key key, Widget child}) {
-    if (_instancia == null){
+    if (_instancia == null) {
       _instancia = new Provider._internal(key: key, child: child);
     }
     return _instancia;
   }
 
-  Provider._internal({Key key, Widget child})
-    : super (key :key, child:child);
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
   @override
-  bool updateShouldNotify(InheritedWidget oldWidget)  => true;
-  
-  static LoginBloc loginProvider ( BuildContext context){
-    return ( context.inheritFromWidgetOfExactType(Provider) as Provider)._loginBloc; 
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static LoginBloc loginProvider(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<Provider>())._loginBloc;
   }
 
-  static LoginBloc logoutProvider ( BuildContext context){
-    return ( context.inheritFromWidgetOfExactType(Provider) as Provider)._loginBloc; 
+  static LoginBloc logoutProvider(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<Provider>())._loginBloc;
   }
 
-  static PedidosBloc pedidosProvider ( BuildContext context){
-    return ( context.inheritFromWidgetOfExactType(Provider) as Provider)._pedidosBloc; 
+  static PedidosBloc pedidosProvider(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<Provider>())
+        ._pedidosBloc;
   }
 }
